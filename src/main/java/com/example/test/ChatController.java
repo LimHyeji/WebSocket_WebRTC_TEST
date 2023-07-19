fa3549a5eedfc462a386b95ad08e6b69a3651de7
임혜지 <hyeji4775@naver.com>
2023-07-19 15:25:54 +0900

    Feat: WebSocket 진행중

--- src/main/java/com/example/test/ChatController.java
+++ src/main/java/com/example/test/ChatController.java
@@ -1,4 +1,11 @@
 package com.example.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ChatController {
+    @GetMapping("/chatrooms")c
+    public String chatrooms(){return "chatrooms";}
+
 }
